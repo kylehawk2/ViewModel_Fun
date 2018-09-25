04ce60d8d4526ac2dd73a4fe870144a019847542
Kyle Hawkins <kylehawk2@gmail.com>
2018-09-24 19:42:46 -0700

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "This is my message";
+            Message msg = new Message()
+            {
+                content = "This message is the default content that has been provided for the root route"
+            };
+            return View("Index", msg);
+        }
+        [Route("numbers")]
+        public IActionResult Numbers()
+        {
+            ViewData["Title"] = "Here are some numbers!";
+            Numbers num = new Numbers()
+            {
+                numList = new List<int> {1,2,3,4,5,6,7,8,9,0}
+            };
+            return View("Numbers", num);
+
+        }
+    }
+}
---
+++ Models/Numbers.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewModelFun.Models
+{
+    public class Numbers
+    {
+        public List<int> numList { get; set; }
+    }
+}
